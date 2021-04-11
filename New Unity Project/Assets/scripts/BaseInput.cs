e69f05895b0ea1bf334fc4994c8223a846ebad0c
jatindahiya027 <jatindahiya027@gmail.com>
2021-04-11 15:31:04 +0530

    systemsEngineering

--- New Unity Project/Assets/scripts/BaseInput.cs
+++ New Unity Project/Assets/scripts/BaseInput.cs
@@ -8,11 +8,5 @@ namespace MLcar.carSystems
         Vector2 GenerateInput();
     }
 
-    public abstract class BaseInput : MonoBehaviour, IInput
-    {
-        /// <summary>
-        /// Override this function to generate an XY input that can be used to steer and control the car.
-        /// </summary>
-        public abstract Vector2 GenerateInput();
-    }
+   
 }
